266da568595b2bc4069f75430a24cbaaf39adfae
damr <damr18@gmail.com>
2020-11-01 21:10:27 -0600

    white btn

--- components/AddItem.js
+++ components/AddItem.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
         margin: 5,
       },
       btn: {
-        backgroundColor: 'red',
+        backgroundColor: 'white',
         padding: 9,
         margin: 5,
       },
